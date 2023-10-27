aa55e2396bea5d0675df757e1b935c2e08b42965
Leo3002 <lucasleonette@gmail.com>
2023-10-27 17:26:34 -0300

    Aula 23 10

---
+++ Aula 23 10/src/components/Card/Card_title.vue
@@ -0,0 +1,19 @@
+<template>
+    <h2 class="p-1 m-1 rounded bg-gray-200">
+        <span class="text-lg border bg-white">
+            {{ icon }}
+        </span>
+        {{ title }}
+    </h2>
+</template>
+<script>
+export default {
+    props: {
+        title: { default: "sem título" }, 
+        icon: {}
+    }
+}
+</script>
+<style lang="">
+    
+</style>
